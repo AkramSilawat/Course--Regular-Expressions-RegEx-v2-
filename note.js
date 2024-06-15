8f17bee9151400e5a906ad53d4a85327efdadaab
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-06-15 23:16:25 +0530

    Regular Expressions:

---
+++ note.js
@@ -0,0 +1,17 @@
+// the colors of the rainbow have many colours and the 
+// rainbow does not have a single colours.
+
+
+
+
+
+//   ----> Regular Expression <---- //
+
+// =======>> Meta-chars <<======= //
+
+//   single char             Quantifiers         position
+//  /d -> 0-9               * -> 0 or more      ^  -> beginning
+//  /w -> a-z A-z 0-9 _     + -> 1 or more      $  -> end
+//  /s -> whitespace        ? -> 0 or 1         /b -> word boundry
+//  .  -> any character     {min, max}
+//                          {n}

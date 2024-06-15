57da7f0426c04cea6a7c9b3fce5f90dd07e523ff
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-06-15 23:40:10 +0530

    Regular Expressions:

--- note.js
+++ note.js
@@ -1,3 +1,15 @@
+
+// These are some phone numbers 917-555-1234.  Also, you 
+// can call me at 646.555.1234 and of course I'm always
+// reachable at (212)867-5309.
+
+// RegEx Code
+
+// \w\w\w      -> These 3 char select  numbers  3 + 3 select s not select
+// \d\w\d\w    -> 646.555t.1234
+// \s\s        -> two whitespace
+
+
 // the colors of the rainbow have many colours and the 
 // rainbow does not have a single colours.
 

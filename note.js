990ea9c8c0657de76be6ff8f8a904bb7add9d222
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-06-19 22:48:29 +0530

    Regular Expressions:

--- note.js
+++ note.js
@@ -27,3 +27,11 @@
 //  /s -> whitespace        ? -> 0 or 1         /b -> word boundry
 //  .  -> any character     {min, max}
 //                          {n}
+
+
+// char-class (alternation)
+
+// [^0-9]{3}        //  match  // all non number
+// l[yi]nk          //  match  // link or lynk
+// \d{3}[-.]\d{3}   //  match  // 222.111 or 888-999
+// \b[A-Z][a-z]*\b  //  match  // Book or The or A or Liitle or Son

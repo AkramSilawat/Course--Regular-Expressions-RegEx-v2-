30ad3a5f7d550ca27a0b45f52a9391b3fddd5b5e
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-06-22 12:41:14 +0530

    Regular Expressions:

---
+++ sketch.js
@@ -0,0 +1,20 @@
+// var textfield;
+// var output;
+// var submit;
+
+// function setup(){
+//     noCanvas();
+//     textfield = select("#input");
+//     textfield.changed(newText);
+//     output = select('#output');
+//     submit = select("#submit");
+//     submit.mousePressed(newText);
+// }
+
+// function newText(){
+//     var s = textfield.value();
+
+//     var r = /\w+@\w+\.(net|com|org)/
+//     createP(r.test(s));
+
+// }

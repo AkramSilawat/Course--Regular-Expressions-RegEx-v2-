faf1fa785a7104a6bdfda164a2488090454605cc
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-06-25 21:35:37 +0530

    Regular Expressions:

--- sketch.js
+++ sketch.js
@@ -1,20 +1,50 @@
-// var textfield;
-// var output;
-// var submit;
+var textfield;
+var output;
+var submit;
 
-// function setup(){
-//     noCanvas();
-//     textfield = select("#input");
-//     textfield.changed(newText);
-//     output = select('#output');
-//     submit = select("#submit");
-//     submit.mousePressed(newText);
-// }
+function setup() {
+    noCanvas();
+    textfield = select("#input");
+    // textfield.changed(newText);
+    output = select('#output');
+    submit = select("#submit");
+    submit.mousePressed(newText);
+}
 
-// function newText(){
-//     var s = textfield.value();
+function newText() {
+    var s = textfield.value();
 
-//     var r = /\w+@\w+\.(net|com|org)/
-//     createP(r.test(s));
+    // var r = /\d{3}[-.]\d{4}/
+    // var r = /\d{3}[-.]\d{4}/g
+    var r = /(\d{3})[-.]\d{4}/g
 
-// }
+    // var mateches = s.match(r);
+    // var r = /\w+@\w+\.(net|com|org)/
+
+
+
+
+    // var result = r.exec(s);
+    // while (result != null) {
+    //     // createP(result);      // -->  /(\d{3})[-.]\d{4}/g
+    //     createP(result[1]);      // -->  /(\d{3})[-.]\d{4}/g
+    //     result = r.exec(s);
+    // }
+
+
+    var results;
+    while( results = r.exec(s)){
+        createP(results[0])
+        console.log
+    }
+
+   
+
+
+    // for (var i = 0; i < mateches.length; i++){
+    //     createP(mateches[i])
+    // }
+    // createP(r.test(s));
+
+}
+
